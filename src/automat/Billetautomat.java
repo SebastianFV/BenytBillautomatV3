46b802a5d32dbd4891ac74ba49ed15946e4d1402
Squid <Squid@LAPTOP-NOS7UNAK.clients.wireless.dtu.dk>
2018-02-28 08:28:43 +0100

    Få rettelser

--- src/automat/Billetautomat.java
+++ src/automat/Billetautomat.java
@@ -1,4 +1,7 @@
 package automat;
+
+import java.util.ArrayList;
+
 /**
  * Model af en simpel billetautomat til enkeltbilletter med én fast pris.
  */
@@ -8,7 +11,6 @@ public class Billetautomat
 	private int balance; // Hvor mange penge kunden p.t. har puttet i automaten
 	private int antalBilletterSolgt; // Antal billetter automaten i alt har solgt
 	private boolean montørtilstand;
-        
 
 	/**
 	 * Opret en billetautomat der sælger billetter til 10 kr.
